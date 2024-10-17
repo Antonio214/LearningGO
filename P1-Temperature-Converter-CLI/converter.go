3ba267ef035e6639085b6d7b9332ceb6e83a032c
Antonio Junior <antonio.junior@quickcomex.com.br>
2024-10-16 22:32:55 -0300

    Implement the conversion functions and pass tests

--- P1-Temperature-Converter-CLI/converter.go
+++ P1-Temperature-Converter-CLI/converter.go
@@ -2,10 +2,10 @@ package converter
 
 // CelsiusToFahrenheit converts Celsius to Fahrenheit.
 func CelsiusToFahrenheit(celsius float64) float64 {
-	return celsius
+	return celsius*9/5 + 32
 }
 
 // FahrenheitToCelsius converts Fahrenheit to Celsius.
 func FahrenheitToCelsius(fahrenheit float64) float64 {
-	return fahrenheit
+	return (fahrenheit - 32) * 5 / 9
 }
